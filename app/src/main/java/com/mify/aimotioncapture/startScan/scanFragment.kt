7a5ef2663400488e1728e621fbd96cfd9995ba4d
MiFy <81895134+XDMiFy@users.noreply.github.com>
2023-04-23 15:31:25 +0600

    1.1

--- app/src/main/java/com/mify/aimotioncapture/startScan/scanFragment.kt
+++ app/src/main/java/com/mify/aimotioncapture/startScan/scanFragment.kt
@@ -1,15 +1,35 @@
 package com.mify.aimotioncapture.startScan
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.mify.aimotioncapture.R
+import com.mify.aimotioncapture.cameraFeature.CamActivity
 
 
 class scanFragment: Fragment() {
+    private val permLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ){ isGranted:Boolean ->
+            if( isGranted){
+                StartScan()
+            }
+        else{
+            //ДЗ: скрыть СтартСкан и показать сообщение
+        }
+     }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,7 +42,19 @@ class scanFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val start_button = view.findViewById<Button>(R.id.ScanStartbutton)
         start_button.setOnClickListener {
-            
+            val permission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            when(permission) {
+                PackageManager.PERMISSION_GRANTED -> {
+                    StartScan()
+                }
+                else -> {
+                    permLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
         }
+    }
+    private fun StartScan() {
+        val intention = Intent(requireContext(), CamActivity::class.java)
+        startActivity(intention)
     }
 }
